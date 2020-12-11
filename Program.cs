321d331e1da8222b35511aa436aac5b1c9f4b239
unknown <dannilorch@gmail.com>
2020-12-11 10:05:19 -0300

    Projeto de console(soma de dois números)

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Projeto_de_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUm;
+            int valorDois;
+
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Digite o primeiro número:");
+            valorUm = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            valorDois = int.Parse(Console.ReadLine());
+
+            int soma = valorUm + valorDois;
+
+            Console.WriteLine("A soma de" +valorUm+ "+" +valorDois+ "=" +soma);
+        }
+    }
+}
